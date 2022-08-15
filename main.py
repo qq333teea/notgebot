4fbcc02e66268b05fad55b1102d7a9472525f4eb
qq333teea <qq333teea@outlook.com>
2022-08-15 14:33:26 +0300

    write

--- main.py
+++ main.py
@@ -7,34 +7,61 @@ import telebot
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
+ex = False
+
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-	pass
+        pass
+
 
 @bot.message_handler(commands=['createNote'])
 def createNote(message):
     try:
-        file = open(str(message.chat.id), 'w')
-        file.close()
-        bot.send_message(message.chat.id, 'ok')
+            file = open(str(message.chat.id), 'w')
+            file.close()
+            bot.send_message(message.chat.id, 'ok')
     except:
-        bot.send_message(message.chat.id, 'error')
+            bot.send_message(message.chat.id, 'error')
+
 
 @bot.message_handler(commands=['cat'])
 def cat(message):
     try:
-        file = open(str(message.chat.id), 'r')
-        note = file.read()
-        bot.send_message(message.chat.id, note)
-        file.close()
+            file = open(str(message.chat.id), 'r')
+            note = file.read()
+            bot.send_message(message.chat.id, note)
+            file.close()
     except:
-        bot.send_message(message.chat.id, 'error')
+            bot.send_message(message.chat.id, 'error')
+
+
+@bot.message_handler(commands=['ex'])
+def ex(message):
+        global ex
+        ex = True
+        bot.send_message(message.chat.id, 'send edited note')
+
+
+@bot.message_handler(content_types=['text'])
+def reply(message):
+        global ex
+        if ex:
+                try:
+                        file = open(str(message.chat.id), 'w')
+                        file.write(message.text)
+                        file.close()
+                        bot.send_message(message.chat.id, 'ok')
+                        ex = False
+                except:
+                        bot.send_message(message.chat.id, 'error')
+
 
 def main():
     try:
-        bot.polling()
+            bot.polling()
     except:
-        main()
+            main()
+
 
 if __name__ == '__main__':
-    main()
+        main()
